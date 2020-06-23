376b8953ba276c173dd5541c50718cd69bdd2648
setocourtney <courtneyjseto@gmail.com>
2020-06-23 15:04:27 -0700

    First Django Project

---
+++ python_intro.py
@@ -0,0 +1,22 @@
+print('Hello' * 3)
+
+# if, elif, else
+if 3 > 2:
+    print(3 > 2)
+elif 3 < 2:
+    print (3 < 2)
+
+
+
+def hi(name):
+    print("Hello " + name)
+
+hi("Tucker")
+
+list = [1, 2, 3, 4]
+
+for num in list:
+    hi(str(num))
+
+for i in range(1,6):
+    hi(str(i))
