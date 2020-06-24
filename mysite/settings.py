84692872be5c96397108ee59ccc2f5a82d7a68f7
setocourtney <courtneyjseto@gmail.com>
2020-06-24 14:25:23 -0700

    fixed settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'djangoblog',
         'USER': 'postgres',
         'PASSWORD': PASSWORD,
